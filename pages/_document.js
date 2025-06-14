613b699f6a6a9b674983a6857aba80ee807f057e
Majid oRc <o0dr.orc0o@gmail.com>
2025-06-15 01:55:35 +0700

    Add PWA support: next-pwa, manifest, icons, meta tags

--- pages/_document.js
+++ pages/_document.js
@@ -9,6 +9,11 @@ export default class MyDocument extends Document {
           <link rel="preconnect" href="https://fonts.googleapis.com" />
           <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
           <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;700&display=swap" rel="stylesheet" />
+          {/* PWA primary color */}
+          <meta name="theme-color" content="#231f3a" />
+          <link rel="manifest" href="/manifest.json" />
+          <link rel="icon" href="/icon-192x192.png" />
+          <link rel="apple-touch-icon" href="/icon-192x192.png" />
         </Head>
         <body>
           <Main />
